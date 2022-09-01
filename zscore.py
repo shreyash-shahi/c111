d7a01975f6661114db89c52ef2b29436af58e4ff
shreyash-shahi <79851781+shreyash-shahi@users.noreply.github.com>
2022-09-01 05:41:54 -0700

    Add files via upload

---
+++ zscore.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import csv
+import plotly.figure_factory as ff
+import statistics
+import random
+import plotly.graph_objects as go
+
+df = pd.read_csv('data1.csv')
+data = df['Math_score'].tolist()
+
+mean = statistics.mean(data)
+std_deviation1 = statistics.stdev(data)
+
+mean_of_sample_1 =statistics.mean(data)
+print('mean of sample:',mean_of_sample_1)
+
+#print('mean of population:',mean)
+#print('std_deviation of population:',std_deviation1)
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0,counter):
+        random_index = random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+
+    mean = statistics.mean(dataset)
+    return mean
+
+mean_list=[]
+for i in range(0,1000):
+    set_of_means = random_set_of_mean(100)
+    mean_list.append(set_of_means)
+    
+std_deviation = statistics.stdev(mean_list)
+
+first_std_deviation_start,first_std_deviation_end = mean-std_deviation,mean+std_deviation
+second_std_deviation_start,second_std_deviation_end = mean-(2*std_deviation),mean+(2*std_deviation)
+third_std_deviation_start,third_std_deviation_end = mean-(3*std_deviation),mean+(3*std_deviation)
+
+print('std1',first_std_deviation_start,first_std_deviation_end)
+print('std2',second_std_deviation_start,second_std_deviation_end)
+print('std3',third_std_deviation_start,third_std_deviation_end)
+
+fig = ff.create_distplot([mean_list],["student marks"],show_hist=False)
+fig.add_trace(go.Scatter(x=[mean,mean],y=[0,0.20],mode="lines",name="Mean"))
+
+fig.add_trace(go.Scatter(x=[first_std_deviation_start,first_std_deviation_start],y=[0,0.17],mode="lines",name="std dev1 start"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end,first_std_deviation_end],y=[0,0.17],mode="lines",name="std dev1 end"))
+
+fig.add_trace(go.Scatter(x=[second_std_deviation_start,second_std_deviation_start],y=[0,0.17],mode="lines",name="std dev2 start"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end,second_std_deviation_end],y=[0,0.17],mode="lines",name="std dev2 end"))
+
+fig.add_trace(go.Scatter(x=[third_std_deviation_start,third_std_deviation_start],y=[0,0.17],mode="lines",name="std dev3 start"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end,third_std_deviation_end],y=[0,0.17],mode="lines",name="std dev3 end"))
+
+fig.show()
+
+zscore = (mean_of_sample_1-mean)/std_deviation
+print('zscore is',zscore)
+    
+
